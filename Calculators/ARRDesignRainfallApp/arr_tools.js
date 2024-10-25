d124f168377e8c3a849a495fc1ce93337cf39282
Nick Cartwright <49052214+enSea00@users.noreply.github.com>
2024-10-25 14:07:33 +1000

    Update arr_tools.js

--- Calculators/ARRDesignRainfallApp/arr_tools.js
+++ Calculators/ARRDesignRainfallApp/arr_tools.js
@@ -15,7 +15,7 @@ function downloadDataHubData(latitude, longitude) {
     // var url = `https://thingproxy.freeboard.io/fetch/https://data.arr-software.org/?lon_coord=${longitude}&lat_coord=${latitude}&type=json&All=1`;
     var url = `https://cors-anywhere.herokuapp.com/https://data.arr-software.org/?lon_coord=${longitude}&lat_coord=${latitude}&type=json&All=1`;
     // var url = `https://corsproxy.io/https://data.arr-software.org/?lon_coord=${longitude}&lat_coord=${latitude}&type=json&All=1`;
-
+    console.log(url)
 
 
     // Trigger the fetch request
