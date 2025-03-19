fd256680b5397528d06d1f6dca0074ba19c513af
Nick Cartwright <49052214+enSea00@users.noreply.github.com>
2025-03-20 07:57:46 +1000

    .

--- DataViewerTools/TheObserver/plotting.js
+++ DataViewerTools/TheObserver/plotting.js
@@ -236,11 +236,11 @@ Object.keys(groupedLocations).forEach((dataType) => {
         });
 
         // Open URL on click (note: `loc.URL` instead of `loc.url`)
-        // marker.on('click', () => {
-        //     if (loc.URL) {
-        //         window.open(loc.URL, '_blank');
-        //     }
-        // });
+        marker.on('click', () => {
+            if (loc.URL) {
+                window.open(loc.URL, '_blank');
+            }
+        });
         
         marker.on('click', function() {
             fetchAndPlotWeatherData(loc.URL); 
@@ -413,4 +413,4 @@ Promise.all(loadingPromises).then(() => {
     // console.log("All map elements are fully loaded.");
 });
 
-// AWS PLOTTER ////////////////////////////////////////////////////////////////////////////////////////
+// END ////////////////////////////////////////////////////////////////////////////////////////
