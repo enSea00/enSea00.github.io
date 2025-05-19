cf1c9609a45a79bbb7882bdb67a332044eb1f4e0
Nick Cartwright <49052214+enSea00@users.noreply.github.com>
2025-05-19 19:50:19 +1000

    .

--- components/footer.js
+++ components/footer.js
@@ -1,26 +1,22 @@
 class Footer extends HTMLElement {
-    constructor() {
-      super();
-    }
-  
-    connectedCallback() {
-      this.innerHTML = `
-      <script src="components/footer.js" type="text/javascript" defer></script>
-        <footer>
-          <nav>
-        
-            <div class="navbar">
-              <div class="container-item-left">
-                  <p style="font-size:10px; text-align:left">&copy; 2023 Nick Cartwright</p>
-              </div>
-              <div class="container-item-right">
-                  <p style="font-size:10px; text-align:right">Wave icon by max.icons</p>
-              </div>
-            </div
-          </nav>
-        </footer>
-      `;
-    }
+  constructor() {
+    super();
   }
-  
-  customElements.define('footer-component', Footer);
+
+  connectedCallback() {
+    const currentYear = new Date().getFullYear(); // Get current year
+    this.innerHTML = `
+      <footer>
+        <nav>
+          <div class="navbar">
+            <div class="container-item-left">
+              <p style="font-size:10px; text-align:left">&copy; ${currentYear} Nick Cartwright</p>
+            </div>
+          </div>
+        </nav>
+      </footer>
+    `;
+  }
+}
+
+customElements.define('footer-component', Footer);
